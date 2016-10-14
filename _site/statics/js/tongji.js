548299b1e4a4c793967962d67b3eb709f553a661
Colonel.MrZhong <1941105185@qq.com>
2016-10-14 17:28:35 +0800

    20161014

---
+++ _site/statics/js/tongji.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?3d782c76ca2846b83dc318a3005df513";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
+});
+
+
+
+
+
+
+
+    
